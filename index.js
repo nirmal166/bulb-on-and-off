b25e99940a550a2129507c0004021c349913ec35
Nirmal Mehra <75833409+nirmal166@users.noreply.github.com>
2021-11-16 08:28:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,197 @@
+/* /* //javascript practice //
+//addion string +number//
+var i=5+"5";
+console.log(i);
+//substraction string+number//
+var z=10-"4";
+console.log(z);//this is bug//
+
+console.log("nirmal"-"mehra");
+console.log(true+true);
+console.log(true-true);
+var h=null;
+console.log(typeof(h));//bug in javascript//
+var i=13/2;
+console.log(i);
+//increment operator//
+var increment=5;
+var inc=increment++;
+console.log(inc);
+console.log(increment);*/
+
+//decrement operator//
+/* var decrement=3;
+var dec=decrement--;
+console.log(dec);
+console.log(decrement); */
+
+//get power of any number we can use expontiation operator//
+//console.log(3**1);//
+
+//write a program of swaping without using third variable// 
+
+/* var ab=2;
+var bc=20;
+var cd=ab=bc;
+console.log(cd); 
+ab=18+2;
+bc=22-20;
+console.log(ab);
+console.log(bc);  */
+
+//year leap year or not //
+/* var year=2024;
+if(year%4==0){
+    console.log("awersome number is leap year")
+}
+else{
+    console.log("not a true numnber is not leap year")
+} */
+
+//use of switch case//
+/* var add=addition;
+switch(add){
+    case 'addition':
+    console.log("the addition of two number is"+9+9);
+} */
+
+//program of the table of 8//
+//the logic of any table in javascript//
+/* var table=8;
+var one=1;
+for(table=8;one<=20;one++){
+    console.log(table + "*" + one + "=" + table*one);
+} */
+
+//fuctions practice in javascript//
+//1st step function defination//
+
+/*  sumoftwo(30, 3);
+sumoftwo(33,2);
+sumoftwo(10,2);
+function sumoftwo(nm,be){
+
+var total=nm+be;
+console.log(total);
+} */
+
+ //functions expressions//
+///    /////
+
+//arrays in javascript //
+
+//var array=["nirmal","mehra","manoj","chouhan"];
+/* console.log(array[2]);
+console.log(array.length); */
+
+/* for(var i=0;i<=array.length;i++){
+    console.log(array[i]);
+} */
+//use of for of loop in javascript//
+/* for(let elements of array){
+    console.log(elements);
+} */
+
+//use of for in loop in javascript//
+/* for(let elements in array){
+    console.log(elements);
+} */
+
+//adject date and time//
+/*  let currdate=new Date();
+console.log(currdate);
+console.log(new Date().toLocaleString());
+console.log(new Date().toString());
+
+var d=new Date(2021,0,5,10,33,30,0);
+console.log(d.toLocaleString()); 
+
+console.log(Math.PI); */
+
+let myBioData;
+let [myfname=nirmal,myname,myfullname] = myBioData;
+console.log(myfname);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
